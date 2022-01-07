a0d8a5f19d1575515ea637dde98585084fe8be0c
Anibal Ventura <anibalventura20@gmail.com>
2022-01-07 17:38:55 -0400

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Anibal Ventura on 7/1/22.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
